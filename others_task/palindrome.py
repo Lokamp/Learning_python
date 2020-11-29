c4d69320e77dd1f901018a6c4df87c7210f8132a
Evgen <sokolov.tt@gmail.com>
2020-11-29 03:36:29 +0300

    finished work on the script

--- others_task/palindrome.py
+++ others_task/palindrome.py
@@ -1,7 +1,13 @@
 string = 'abttba'
 
 if len(string) % 2 == 0:
-    two_parts = len(string) / 2
-    print(f'This is a palindrome, first part {string[:int(two_parts)]}, second part {string[int(two_parts):]}')
+    split_word_into_two_parts = len(string) / 2
+    first_part = string[:int(split_word_into_two_parts)]
+    second_part = string[int(split_word_into_two_parts):][::-1]
+    if first_part == second_part:
+        print(f'This is a palindrome, first part {first_part}, second part {second_part}')
+    else:
+        print('This is not palindrome')
 else:
     print('A palindrome can only consist of an even number of elements by definition')
+
