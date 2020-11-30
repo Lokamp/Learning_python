18a9131f71ea5c0f80af74bc1de26c1a05254923
Evgen <sokolov.tt@gmail.com>
2020-11-30 03:36:24 +0300

    rewrote not working script

--- others_task/telephone_number.py
+++ others_task/telephone_number.py
@@ -1,9 +1,7 @@
-# Проверка ввода телефоного номера
+# Проверка ввода телефонного номера
 
 telephone_number = input('Введите номер телефона без +7, используйте только цифры, пример: 9001000000 ')
 
-while True:
-    if telephone_number.isdigit() and len(telephone_number) == 10:
-        print(f'Ваш номер телефона +7{telephone_number}')
-    else:
-        print('Вы неверно ввели номер телефона, необходимо ввести 10 цифр, пример: 9001000000 ')
+while not (telephone_number.isdigit() and len(telephone_number) == 10):
+    telephone_number = input('Вы неверно ввели номер телефона, необходимо ввести 10 цифр, пример: 9001000000 ')
+print(f'Ваш номер телефона +7{telephone_number}')
