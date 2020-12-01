df8d8b91d29f69bcc2d1b7777abb5893f4efd686
Evgen <sokolov.tt@gmail.com>
2020-12-02 00:49:10 +0300

    add script - code execution time

---
+++ others_task/code_execution_time.py
@@ -0,0 +1,12 @@
+import timeit
+
+# not my code
+code_to_test = """
+a = range(100000)
+b = []
+for i in a:
+    b.append(i*2)
+"""
+
+elapsed_time = timeit.timeit(code_to_test, number=100) / 100
+print(elapsed_time)
