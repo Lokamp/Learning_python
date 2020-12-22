993de59dffe81802e3e2477f69f99e5e7336f9a3
Evgen <sokolov.tt@gmail.com>
2020-12-22 21:21:57 +0300

    del txt file

--- py_checkio_org/3_task_solution.py
+++ py_checkio_org/3_task_solution.py
@@ -1,6 +1,9 @@
+# In a given list the first element should become the last one.
+# An empty list or list with only one element should stay the same.
+
 from typing import Iterable
 
-
+# first solution
 def replace_first(items: list) -> Iterable:
     if len(items) > 1:
         first_item = items.pop(0)
@@ -11,5 +14,5 @@ def replace_first(items: list) -> Iterable:
 
 
 # second solution
-# def replace_first(items: list) -> list:
-#     return items[1:] + items[:1]
+def replace_first(items: list) -> list:
+    return items[1:] + items[:1]
