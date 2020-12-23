cbf0e83127be4f8aa762d084f1a1b77203e2c82f
Evgen <sokolov.tt@gmail.com>
2020-12-23 15:41:47 +0300

    add func min and refactoring func max

--- others_task/function_max.py
+++ others_task/function_max.py
@@ -3,12 +3,24 @@
 
 
 def max_custom(items: list) -> int:
-    max_item = [items[0]]
+    max_item = 0
     for item in items:
-        if item > max_item[0]:
-            max_item[0] = item
+        if item > max_item:
+            max_item = item
 
-    return max_item[0]
+    return max_item
 
 
 print(max_custom([-8, 66, 6, -4, 9, -6, -1, 5, -11, 44]))
+
+
+def max_custom_(items: list) -> int:
+    max_item = float('inf')
+    for item in items:
+        if item < max_item:
+            max_item = item
+
+    return max_item
+
+
+print(max_custom_([-8, 66, 6, -4, 9, -6, -1, 5, -11, 44]))
